5f2f4d9fcc10c493435a2e604e3ffe6f7e9377b6
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-10-28 17:27:57 -0400

    use bundle for now

--- nix/server.nix
+++ nix/server.nix
@@ -2,27 +2,13 @@
   name = "cliquet";
   version = "1.0.0";
 
-  node-modules = pkgs.stdenvNoCC.mkDerivation {
-    inherit version;
-    name = "${name}-node-modules";
-    src = ../.;
-    dontFixup = true;
-    buildInputs = [pkgs.bun];
-    buildPhase = "bun i";
-    installPhase = "cp -r node_modules $out";
-    outputHashAlgo = "sha256";
-    outputHashMode = "recursive";
-    outputHash = "sha256-8JdHU5JeBJJ9VAg8wfY0ekZPYQnbvg72d+8cA+tcb7Q=";
-  };
-
   bundle = pkgs.stdenvNoCC.mkDerivation {
     inherit version;
     name = "${name}-bundle";
     src = ../.;
     buildInputs = with pkgs; [bun];
     buildPhase = ''
-      cp -r ${node-modules} node_modules
-      bun run build
+      cp ${../dist/main.js} dist/main.js
     '';
     installPhase = ''
       cp dist/main.js $out
