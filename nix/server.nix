b9e21e63830652b7a32e33bc7fa312ff6f5a10b3
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-10-29 03:25:33 -0400

    add bundle

--- nix/server.nix
+++ nix/server.nix
@@ -1,19 +1,5 @@
 {pkgs, ...}: let
-  name = "cliquet";
-  version = "1.0.0";
-
-  bundle = pkgs.stdenvNoCC.mkDerivation {
-    inherit version;
-    name = "${name}-bundle";
-    src = ../.;
-    buildInputs = with pkgs; [bun];
-    buildPhase = ''
-      cp ${../dist/main.js} dist/main.js
-    '';
-    installPhase = ''
-      cp dist/main.js $out
-    '';
-  };
+  bundle = ../dist/server.js;
 in
   pkgs.writeShellScriptBin "cliquet" ''
     ${pkgs.lib.getExe pkgs.bun} ${bundle} "$@"
